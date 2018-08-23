5cf91e9c716cd83ccc62eada4b29b4d1237038bd
degendeg <sebbed89@hotmail.com>
2018-08-23 10:33:07 +0200

    Added melee weapons Added cache to /items Fixes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var basicAuth = require('express-basic-auth');
+var mcache = require('memory-cache');
 var Items = require('warframe-items');
 var items = new Items();
 var port = process.env.PORT || 3005;
@@ -12,7 +13,26 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/default.html');
 });
 
-app.get('/items', basicAuth({
+var cache = (duration) => {
+	return (req, res, next) => {
+		let key = '__express__' + req.originalUrl || req.url;
+		let cachedBody = mcache.get(key);
+		if (cachedBody) {
+			res.send(cachedBody);
+			return;
+		}
+		else {
+			res.sendResponse = res.send;
+			res.send = (body) => {
+				mcache.put(key, body, duration * 1000);
+				res.sendResponse(body);
+			}
+			next();
+		}
+	}
+}
+
+app.get('/items', cache(10), basicAuth({
   users: {
     'urbz': 'seiko12345'
   },
